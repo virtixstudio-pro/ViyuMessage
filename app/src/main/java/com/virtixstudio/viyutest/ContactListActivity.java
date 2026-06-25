32063a0ff545d9ed40aa578931008fe3333e8982
Persévérance <ton_email_github@gmail.com>
2026-06-26 00:46:58 +0300

    Structure propre : declaration des activitees, profils, contacts et manifestes synchronises

---
+++ app/src/main/java/com/virtixstudio/viyutest/ContactListActivity.java
@@ -0,0 +1,9 @@
+package com.virtixstudio.viyutest;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class ContactListActivity extends AppCompatActivity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+}
+}
