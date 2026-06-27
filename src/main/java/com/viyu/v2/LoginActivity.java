f5e6006e0f7c241b745b45c6a2babcf90c69f072
Persévérance <ton_email_github@gmail.com>
2026-06-27 16:24:22 +0300

    Forçage du démarrage direct sur ChatActivity via cat EOF

--- src/main/java/com/viyu/v2/LoginActivity.java
+++ src/main/java/com/viyu/v2/LoginActivity.java
@@ -2,13 +2,19 @@ package com.viyu.v2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, ChatActivity.class));
-        finish();
+        Log.d("ViyuDebug", "Forçage du démarrage direct vers ChatActivity");
+        try {
+            startActivity(new Intent(LoginActivity.this, ChatActivity.class));
+            finish();
+        } catch (Exception e) {
+            Log.e("ViyuCritical", "Impossible de lancer ChatActivity", e);
+        }
     }
 }
